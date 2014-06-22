0f11969f24b3661db8f9d8df4c46d5b53431a433
Chalermchon Samana <appcodev@gmail.com>
2014-06-22 23:59:36 +0700

    first time commit

---
+++ Ex1_HelloMyApp/Ex1_HelloMyAppTests/Ex1_HelloMyAppTests.m
@@ -0,0 +1,34 @@
+//
+//  Ex1_HelloMyAppTests.m
+//  Ex1_HelloMyAppTests
+//
+//  Created by Chalermchon Samana on 6/22/14.
+//  Copyright (c) 2014 Onzondev Innovation Co., Ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ex1_HelloMyAppTests : XCTestCase
+
+@end
+
+@implementation Ex1_HelloMyAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
