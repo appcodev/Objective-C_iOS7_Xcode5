c5c7bbbe9084226446bdd819b75b2a3382b7c077
Chalermchon Samana <appcodev@gmail.com>
2014-06-24 06:33:05 +0700

    + first commit

---
+++ Ex6_ScoreTable/Ex6_ScoreTableTests/Ex6_ScoreTableTests.m
@@ -0,0 +1,34 @@
+//
+//  Ex6_ScoreTableTests.m
+//  Ex6_ScoreTableTests
+//
+//  Created by Chalermchon Samana on 6/24/14.
+//  Copyright (c) 2014 Onzondev Innovation Co., Ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ex6_ScoreTableTests : XCTestCase
+
+@end
+
+@implementation Ex6_ScoreTableTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
